a6b7304f8bf6768c8d7eb303b850d22e15d589d5
Elizabeth Cano <ely.cano2033@gmail.com>
2022-04-09 17:06:29 -0400

    added gitignore

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3001;
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: "giantredrooster",
+  secret: "secret",
   cookie: {
     // Session will automatically expire in 10 minutes
     expires: 100 * 60 * 1000,
